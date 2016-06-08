bc633e8e8de0107127196a3379bed7b27b5b8936
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-08 11:12:43 -0500

    Fix server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,26 @@
+// var http=require('http');
+// http.createServer(function(req,res){
+//   res.writeHead(200);
+//   resp.write("Hello world!");
+//   res.end();
+// }).listen(3000);
+//
+// console.log('listening on port8080');
+
 // require express
 var express = require('express');
 // create a new express app
 var app = express();
 // require path
-var path = require('path');
+// var path = require('path');
 /// FIRST RUN: npm install body-parser --save
 // these are needed for POST
-var bodyParser = require('body-parser');
-// don't use extended as
-var urlencodedParser = bodyParser.urlencoded({extended:false});
+// var bodyParser = require('body-parser');
+// // // don't use extended as
+// var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 // spin up the server
-var server = app.listen(process.env.PORT || 8080, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   console.log('server listening on port');
 });
 
@@ -19,7 +28,7 @@ var server = app.listen(process.env.PORT || 8080, function(){
 app.get('/', function( req, res ){
   console.log( 'hello world from base url get' );
   res.writeHead(200);
-  res.write('we are in the base url');
+  res.write('Hello world!');
   // will hang on load if not ended
   res.end();
 });
