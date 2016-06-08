d8f2f38a71dc42d98b7b968cb3cf33b214661df2
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-08 11:15:29 -0500

    Clean up code

--- server/app.js
+++ server/app.js
@@ -1,23 +1,7 @@
-// var http=require('http');
-// http.createServer(function(req,res){
-//   res.writeHead(200);
-//   resp.write("Hello world!");
-//   res.end();
-// }).listen(3000);
-//
-// console.log('listening on port8080');
-
 // require express
 var express = require('express');
 // create a new express app
 var app = express();
-// require path
-// var path = require('path');
-/// FIRST RUN: npm install body-parser --save
-// these are needed for POST
-// var bodyParser = require('body-parser');
-// // // don't use extended as
-// var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 // spin up the server
 var server = app.listen(process.env.PORT || 3000, function(){
@@ -32,6 +16,3 @@ app.get('/', function( req, res ){
   // will hang on load if not ended
   res.end();
 });
-
-// set up "public" folder for static files
-// app.use(express.static('public'));
